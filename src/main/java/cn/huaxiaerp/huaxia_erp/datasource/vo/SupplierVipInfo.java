a384cb3874d9861aa9324792b723d9ba38b656a1
啊帽 <45966123@qq.com>
2023-05-31 17:16:01 +0800

    动态sql获取会员/供应商/客户

---
+++ src/main/java/cn/huaxiaerp/huaxia_erp/datasource/vo/SupplierVipInfo.java
@@ -0,0 +1,41 @@
+package cn.huaxiaerp.huaxia_erp.datasource.vo;
+
+import java.math.BigDecimal;
+
+public class SupplierVipInfo {
+    /**
+     * 会员卡号
+     */
+    private String accountNumber;
+    /**
+     * 联系人
+     */
+    private String Contacts;
+    /**
+     * 手机号
+     */
+    private String Telephone;
+    /**
+     * 联系电话
+     */
+    private String PhoneNum;
+    /**
+     *邮箱
+     */
+    private String email;
+    /**
+     * 预付款
+     */
+    private BigDecimal beginNeedGet;
+    /**
+     * 排序
+     */
+    private String sort;
+    /**
+     * 状态
+     */
+    private String enabled;
+
+
+
+}
