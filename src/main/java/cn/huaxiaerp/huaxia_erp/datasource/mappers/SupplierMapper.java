dca1afe1f828625fa31bc97dcdf5b5be19c0db04
啊帽 <45966123@qq.com>
2023-05-31 14:21:40 +0800

    删除mapper_xml文件夹增加供应商/客户/会员查询

--- src/main/java/cn/huaxiaerp/huaxia_erp/datasource/mappers/SupplierMapper.java
+++ src/main/java/cn/huaxiaerp/huaxia_erp/datasource/mappers/SupplierMapper.java
@@ -4,6 +4,8 @@ package cn.huaxiaerp.huaxia_erp.datasource.mappers;
 import cn.huaxiaerp.huaxia_erp.datasource.po.Supplier;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 
+import java.util.List;
+
 /**
  * <p>
  * 供应商/客户信息表 Mapper 接口
@@ -14,4 +16,14 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
  */
 public interface SupplierMapper extends BaseMapper<Supplier> {
 
+    /**
+     * 动态sql获取供应商信息
+     * @param supplier 供应商名称
+     * @param telephone 手机号码
+     * @param phoneNum 联系电话
+     * @param type 供应商/客户
+     * @return
+     */
+    List<Supplier> getSupplierLikeSupplierOrTelephoneOrPhoneNumber(String type, String supplier, String telephone, String phoneNum);
+    List<Supplier> getAll();
 }
