9cbb325f91276ae0d85842173d7cd50828541b7b
kangkang <1622923556@qq.com>
2023-06-01 10:57:34 +0800

    新增swagger配置类，启动类

--- src/main/java/cn/huaxiaerp/huaxia_erp/HuaxiaErpApplication.java
+++ src/main/java/cn/huaxiaerp/huaxia_erp/HuaxiaErpApplication.java
@@ -4,13 +4,15 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
- * * kjc
+ * 启动类
  */
 @SpringBootApplication
 @MapperScan("cn.huaxiaerp.huaxia_erp.datasource.mappers")
 @ComponentScan("cn.huaxiaerp")
+@EnableSwagger2
 public class HuaxiaErpApplication {
 
     public static void main(String[] args) {
