185d442c6db5298122765fe0d07411fe02974d57
LAPTOP-UQ6DCSAI\MI <123456>
2023-06-01 15:58:37 +0800

    漏提交了一个零售查询列表的mapper

--- src/main/java/cn/huaxiaerp/huaxia_erp/datasource/mappers/DepotHeadMapper.java
+++ src/main/java/cn/huaxiaerp/huaxia_erp/datasource/mappers/DepotHeadMapper.java
@@ -1,7 +1,12 @@
 package cn.huaxiaerp.huaxia_erp.datasource.mappers;
 
 import cn.huaxiaerp.huaxia_erp.datasource.po.DepotHead;
+import cn.huaxiaerp.huaxia_erp.datasource.vo.DepotHeadVo4;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
 
 /**
  * <p>
@@ -11,6 +16,14 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
  * @author kjc
  * @since 2023-05-31
  */
+@Mapper
 public interface DepotHeadMapper extends BaseMapper<DepotHead> {
 
+    @Select("select js.supplier,default_number,jm.`name`,jm.standard,jm.model,jdh.oper_time,ju.username,jdi.oper_number,jdi.all_price from jsh_depot_head as jdh\n" +
+            "JOIN jsh_depot_item as jdi on jdh.id = jdi.header_id\n" +
+            "LEFT JOIN jsh_supplier AS js ON js.id = jdh.organ_id\n" +
+            "join jsh_material as jm on jdi.material_id = jm.id\n" +
+            "join jsh_user as ju on ju.id = jdh.creator\n" +
+            "where sub_type = '零售' and jdh.type = '出库'")
+    public List<DepotHeadVo4> getAll();
 }
