8b471d5af4a05457b46f28a44aba5d261a7ae85d
啊帽 <45966123@qq.com>
2023-06-01 11:21:36 +0800

    啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊

--- src/main/java/cn/huaxiaerp/huaxia_erp/controller/TestController/TestController.java
+++ src/main/java/cn/huaxiaerp/huaxia_erp/controller/TestController/TestController.java
@@ -1,17 +1,19 @@
 package cn.huaxiaerp.huaxia_erp.controller.TestController;
 
+import cn.huaxiaerp.huaxia_erp.common.response.Result;
+import cn.huaxiaerp.huaxia_erp.datasource.dto.supplierParme;
 import cn.huaxiaerp.huaxia_erp.datasource.po.Depot;
 import cn.huaxiaerp.huaxia_erp.datasource.po.Supplier;
+import cn.huaxiaerp.huaxia_erp.datasource.vo.SupplierVendorAndUser;
 import cn.huaxiaerp.huaxia_erp.service.DepotService;
 import cn.huaxiaerp.huaxia_erp.service.SupplierService;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -23,38 +25,51 @@ public class TestController {
     private DepotService depotService;
 
     @GetMapping("/getAll")
-    public JSONArray getAll(){
-        List<Supplier> supplierLikeSupplierOrTelephoneOrPhoneNumber = supplierService.getSupplierLikeSupplierOrTelephoneOrPhoneNumber("会员","", "", "");
+    public List<SupplierVendorAndUser> getAll(supplierParme supplierVendorAndUser){
+        List<Supplier> supplierLikeSupplierOrTelephoneOrPhoneNumber = supplierService.getSupplierLikeSupplierOrTelephoneOrPhoneNumber("供应商",supplierVendorAndUser.getSupplier(), supplierVendorAndUser.getTelephone(), supplierVendorAndUser.getPhoneNum());
         System.out.println("supplierLikeSupplierOrTelephoneOrPhoneNumber的结果");
         System.out.println(supplierLikeSupplierOrTelephoneOrPhoneNumber);
-        JSONArray arr = new JSONArray();
-        JSONArray dataArray = new JSONArray();
+
+        List<SupplierVendorAndUser> supplierVendorAndUserList = new ArrayList<>();
         if (null != supplierLikeSupplierOrTelephoneOrPhoneNumber) {
             for (Supplier supplier : supplierLikeSupplierOrTelephoneOrPhoneNumber) {
-                JSONObject item = new JSONObject();
-                item.put("id", supplier.getId());
+                SupplierVendorAndUser item = new SupplierVendorAndUser();
+                item.setId(supplier.getId().toString());
                 //供应商名称
-                item.put("supplier", supplier.getSupplier());
-                item.put("contacts",supplier.getContacts());
-                item.put("telephone",supplier.getTelephone());
-                item.put("phone_num",supplier.getPhoneNum());
-                item.put("email",supplier.getEmail());
-                item.put("begin_need_pay",supplier.getBeginNeedPay());
-                item.put("all_need_pay",supplier.getAllNeedPay());
-                item.put("tax_rate",supplier.getTaxRate());
-                item.put("enabled",supplier.getEnabled());
-                dataArray.add(item);
+                item.setSupplier(supplier.getSupplier());
+                item.setContacts(supplier.getContacts());
+                item.setTelephone(supplier.getTelephone());
+                item.setPhoneNum(supplier.getPhoneNum());
+                item.setEmail(supplier.getEmail());
+                item.setBeginNeedGet(supplier.getBeginNeedPay());
+                item.setEndNeedGet((supplier.getAllNeedPay().add(supplier.getBeginNeedPay())));
+                item.setTaxRate(supplier.getTaxRate());
+                item.setEnabled(supplier.getEnabled());
+                System.out.println("添加完成的值：");
+                System.out.println(item);
+                supplierVendorAndUserList.add(item);
             }
         }
-
-        arr = dataArray;
-        System.out.println(arr);
-        return arr;
+        return supplierVendorAndUserList;
     }
 
     @GetMapping("/getAll2")
     public List<Depot> getAllLikeNameOrLikeRemake(){
         return depotService.getAllLikeNameOrLikeRemake("仓库","");
+    }
+
+    //根据id批量设置enable
+    @GetMapping("/batchUpdateEnable")
+    public boolean batchUpdateEnable(@RequestParam("ids") List<Integer> ids, @RequestParam("enable") Boolean enabled) {
+
+        System.out.println("ids结果：");
+        System.out.println(ids);
+        try {
+            supplierService.batchUpdateEnable(ids, enabled);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
     }
 
 }
